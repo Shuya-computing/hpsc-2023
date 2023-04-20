a6b1ee8bbc233c11c797a329d6c35c296c20cb9c
Shuya-computing <64950082+Shuya-computing@users.noreply.github.com>
2023-04-21 01:16:38 +0900

    Lecture2-3

--- 02_openmp/14_bucket_sort.cpp
+++ 02_openmp/14_bucket_sort.cpp
@@ -12,12 +12,29 @@ int main() {
   }
   printf("\n");
 
-  std::vector<int> bucket(range,0); 
+  std::vector<int> bucket(range,0);
   for (int i=0; i<n; i++)
     bucket[key[i]]++;
   std::vector<int> offset(range,0);
-  for (int i=1; i<range; i++) 
-    offset[i] = offset[i-1] + bucket[i-1];
+  std::vector<int> offset2(range, 0);
+#pragma omp parallel for
+  for(int i=0; i<range; i++)
+      offset2[i]=bucket[i];
+#pragma omp parallel
+  for(int j=1; j<range; j<<=1){
+#pragma omp for
+    for(int i=0; i<range; i++){
+      offset[i]=offset2[i];
+    }
+#pragma omp for
+    for(int i=j; i<range; i++)
+      offset2[i] += offset[i-j];
+  }
+#pragma omp parallel for
+  for(int i=0; i<range; i++){
+      offset[i]=offset2[i]-bucket[i];
+  }
+#pragma omp parallel for
   for (int i=0; i<range; i++) {
     int j = offset[i];
     for (; bucket[i]>0; bucket[i]--) {
